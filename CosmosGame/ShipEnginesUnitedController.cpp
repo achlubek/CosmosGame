a3698be3048a66ec1a0d0aa487c3586032ac89e5
Adrian Chlubek <luby.pl@gmail.com>
2017-12-15 11:46:38 +0100

    Engine controller that will fit a lot better with keymapping and custom ships

---
+++ CosmosGame/ShipEnginesUnitedController.cpp
@@ -0,0 +1,120 @@
+#include "stdafx.h"
+#include "ShipEnginesUnitedController.h"
+
+
+ShipEnginesUnitedController::ShipEnginesUnitedController()
+{
+}
+
+
+ShipEnginesUnitedController::~ShipEnginesUnitedController()
+{
+}
+
+void ShipEnginesUnitedController::setLinearThrust(glm::dvec3 lt)
+{
+    linearThrust = lt;
+}
+
+void ShipEnginesUnitedController::setAngularThrust(glm::dvec3 at)
+{
+    angularThrust = at;
+}
+
+
+void ShipEnginesUnitedController::setEnginesLinearPositiveX(std::vector<SpaceShipEngine*> engines)
+{
+    enginesLinearPositiveX = engines;
+}
+
+void ShipEnginesUnitedController::setEnginesLinearNegativeX(std::vector<SpaceShipEngine*> engines)
+{
+    enginesLinearNegativeX = engines;
+}
+
+
+void ShipEnginesUnitedController::setEnginesLinearPositiveY(std::vector<SpaceShipEngine*> engines)
+{
+    enginesLinearPositiveY = engines;
+}
+
+void ShipEnginesUnitedController::setEnginesLinearNegativeY(std::vector<SpaceShipEngine*> engines)
+{
+    enginesLinearNegativeY = engines;
+}
+
+
+void ShipEnginesUnitedController::setEnginesLinearPositiveZ(std::vector<SpaceShipEngine*> engines)
+{
+    enginesLinearPositiveZ = engines;
+}
+
+void ShipEnginesUnitedController::setEnginesLinearNegativeZ(std::vector<SpaceShipEngine*> engines)
+{
+    enginesLinearNegativeZ = engines;
+}
+
+
+
+void ShipEnginesUnitedController::setEnginesAngularPositiveX(std::vector<SpaceShipEngine*> engines)
+{
+    enginesAngularPositiveX = engines;
+}
+
+void ShipEnginesUnitedController::setEnginesAngularNegativeX(std::vector<SpaceShipEngine*> engines)
+{
+    enginesAngularNegativeX = engines;
+}
+
+
+void ShipEnginesUnitedController::setEnginesAngularPositiveY(std::vector<SpaceShipEngine*> engines)
+{
+    enginesAngularPositiveY = engines;
+}
+
+void ShipEnginesUnitedController::setEnginesAngularNegativeY(std::vector<SpaceShipEngine*> engines)
+{
+    enginesAngularNegativeY = engines;
+}
+
+
+void ShipEnginesUnitedController::setEnginesAngularPositiveZ(std::vector<SpaceShipEngine*> engines)
+{
+    enginesAngularPositiveZ = engines;
+}
+
+void ShipEnginesUnitedController::setEnginesAngularNegativeZ(std::vector<SpaceShipEngine*> engines)
+{
+    enginesAngularNegativeZ = engines;
+}
+
+void ShipEnginesUnitedController::updateEnginesThrust()
+{
+    // ship modules precentages are clamped on being set, to it makes this class simplier
+    // linear thrust:
+    setAllEnginesInVectorPower(enginesLinearPositiveX, linearThrust.x);
+    setAllEnginesInVectorPower(enginesLinearNegativeX, -linearThrust.x);
+
+    setAllEnginesInVectorPower(enginesLinearPositiveY, linearThrust.y);
+    setAllEnginesInVectorPower(enginesLinearNegativeY, -linearThrust.y);
+
+    setAllEnginesInVectorPower(enginesLinearPositiveZ, linearThrust.z);
+    setAllEnginesInVectorPower(enginesLinearNegativeZ, -linearThrust.z);
+
+    // angular thrust:
+    setAllEnginesInVectorPower(enginesAngularPositiveX, angularThrust.x);
+    setAllEnginesInVectorPower(enginesAngularNegativeX, -angularThrust.x);
+
+    setAllEnginesInVectorPower(enginesAngularPositiveY, angularThrust.y);
+    setAllEnginesInVectorPower(enginesAngularNegativeY, -angularThrust.y);
+
+    setAllEnginesInVectorPower(enginesAngularPositiveZ, angularThrust.z);
+    setAllEnginesInVectorPower(enginesAngularNegativeZ, -angularThrust.z);
+}
+
+void ShipEnginesUnitedController::setAllEnginesInVectorPower(std::vector<SpaceShipEngine*> &engines, double power)
+{
+    for (int i = 0; i < engines.size(); i++) {
+        engines[i]->setPowerPercentage(power);
+    }
+}
---
+++ CosmosGame/ShipEnginesUnitedController.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "spaceship\SpaceShipEngine.h"
+class ShipEnginesUnitedController
+{
+public:
+    ShipEnginesUnitedController();
+    ~ShipEnginesUnitedController();
+
+    void setLinearThrust(glm::dvec3 lt);
+    void setAngularThrust(glm::dvec3 at);
+
+    void setEnginesLinearPositiveX(std::vector<SpaceShipEngine*> engines);
+    void setEnginesLinearNegativeX(std::vector<SpaceShipEngine*> engines);
+    void setEnginesLinearPositiveY(std::vector<SpaceShipEngine*> engines);
+    void setEnginesLinearNegativeY(std::vector<SpaceShipEngine*> engines);
+    void setEnginesLinearPositiveZ(std::vector<SpaceShipEngine*> engines);
+    void setEnginesLinearNegativeZ(std::vector<SpaceShipEngine*> engines);
+    void setEnginesAngularPositiveX(std::vector<SpaceShipEngine*> engines);
+    void setEnginesAngularNegativeX(std::vector<SpaceShipEngine*> engines);
+    void setEnginesAngularPositiveY(std::vector<SpaceShipEngine*> engines);
+    void setEnginesAngularNegativeY(std::vector<SpaceShipEngine*> engines);
+    void setEnginesAngularPositiveZ(std::vector<SpaceShipEngine*> engines);
+    void setEnginesAngularNegativeZ(std::vector<SpaceShipEngine*> engines);
+
+    void updateEnginesThrust();
+
+private:
+    glm::dvec3 linearThrust;
+    glm::dvec3 angularThrust;
+    std::vector<SpaceShipEngine*> enginesLinearPositiveX;
+    std::vector<SpaceShipEngine*> enginesLinearNegativeX;
+
+    std::vector<SpaceShipEngine*> enginesLinearPositiveY;
+    std::vector<SpaceShipEngine*> enginesLinearNegativeY;
+
+    std::vector<SpaceShipEngine*> enginesLinearPositiveZ;
+    std::vector<SpaceShipEngine*> enginesLinearNegativeZ;
+
+    std::vector<SpaceShipEngine*> enginesAngularPositiveX;
+    std::vector<SpaceShipEngine*> enginesAngularNegativeX;
+
+    std::vector<SpaceShipEngine*> enginesAngularPositiveY;
+    std::vector<SpaceShipEngine*> enginesAngularNegativeY;
+
+    std::vector<SpaceShipEngine*> enginesAngularPositiveZ;
+    std::vector<SpaceShipEngine*> enginesAngularNegativeZ;
+
+    void setAllEnginesInVectorPower(std::vector<SpaceShipEngine*> &engines, double power);
+};
+
